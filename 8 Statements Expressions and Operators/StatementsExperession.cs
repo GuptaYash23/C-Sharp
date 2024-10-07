8c3791c33bca77f0a30928ad9b46840e19923f59
Yash Gupta <yashg.workmail@gmail.com>
2024-10-08 00:02:08 +0530

    Add replace files

---
+++ 8 Statements Expressions and Operators/StatementsExperession.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            const float PI = 3.14F;
+            float radius = 5;
+            float area = PI*radius*radius;
+
+            Console.WriteLine("The area of the circle is : "+ area);   // The area of the circle is 78.5
+            Console.ReadLine();
+        }
+    }
+}
